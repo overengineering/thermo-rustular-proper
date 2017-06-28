29b0fad840d37506f54731ccbbe6b3ff35f97800
Stefano Chiodino <louderth@gmail.com>
2017-06-28 18:58:34 +0100

    Beginning pass wordgen

---
+++ src/api_password_generator.rs
@@ -0,0 +1,8 @@
+pub fn build_password<'a>() -> &'a str {
+    "NICEGAMEOFCHESS"
+}
+
+// #[test]
+// fn password_is_correct() {
+    
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod api_password_generator;
+
 struct LaunchResponse {
     result: ApiResultState,
     message: String,
@@ -13,23 +15,34 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn launch(password: String) -> Result<LaunchResponse, String> {
-    Ok(LaunchResponse {
-           result: ApiResultState::Failure,
-           message: "".to_string(),
-       })
+fn launch(password: &str) -> Result<LaunchResponse, String> {
+    if password == "NICEGAMEOFCHESS" {
+        Ok(LaunchResponse {
+               result: ApiResultState::Success,
+               message: "".to_string(),
+           })
+    } else {
+        Ok(LaunchResponse {
+               result: ApiResultState::Failure,
+               message: "".to_string(),
+           })
+    }
 }
 
 #[test]
 fn test_launch_with_bad_password() {
-    let launch_response = launch("password".to_string());
+    let launch_response = launch("password").unwrap();
 
-    assert_eq!(launch_response.unwrap().result, ApiResultState::Failure);
+
+
+    assert_eq!(launch_response.result, ApiResultState::Failure);
+    assert_eq!(launch_response.message, "Invalid Launch Code");
 }
 
 #[test]
 fn test_launch_with_good_password() {
-    let launch_response = launch("NICEGAMEOFCHESS".to_string());
+    let launch_response = launch(api_password_generator::build_password()).unwrap();
 
-    assert_eq!(launch_response.unwrap().result, ApiResultState::Success);
+    assert_eq!(launch_response.result, ApiResultState::Success);
+    assert_ne!(launch_response.message, "Invalid Launch Code");
 }
