db81e054c4428df0efb6818df5b077bf77d4e5a8
Stefano Chiodino <louderth@gmail.com>
2017-06-28 20:01:15 +0100

    actual api call

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
+extern crate reqwest;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
 mod api_password_generator;
 
+#[derive(Deserialize)]
 struct LaunchResponse {
+    #[serde(rename = "Result")]
     result: ApiResultState,
+    #[serde(rename = "Message")]
     message: String,
 }
 
-#[derive(Debug,PartialEq)]
+#[derive(Debug,PartialEq,Deserialize)]
 enum ApiResultState {
     Success,
     Failure,
@@ -15,25 +24,20 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn launch(password: &str) -> Result<LaunchResponse, String> {
-    if password == "NICEGAMEOFCHESS" {
-        Ok(LaunchResponse {
-               result: ApiResultState::Success,
-               message: "".to_string(),
-           })
-    } else {
-        Ok(LaunchResponse {
-               result: ApiResultState::Failure,
-               message: "".to_string(),
-           })
-    }
+fn launch(president_password: &str) -> Result<LaunchResponse, reqwest::Error> {
+    let client = reqwest::Client::new().unwrap();
+
+    let launch_response: LaunchResponse = client.post(&("http://gitland.azurewebsites.net:80/api/warheads/launch?launchCode="
+                    .to_owned() + president_password))
+        .send()?
+        .json()?;
+
+    Ok(launch_response)
 }
 
 #[test]
 fn test_launch_with_bad_password() {
     let launch_response = launch("password").unwrap();
-
-
 
     assert_eq!(launch_response.result, ApiResultState::Failure);
     assert_eq!(launch_response.message, "Invalid Launch Code");
