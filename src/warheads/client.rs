5d61d89d73fdc4285e586ddcf9e1b1fb7494096f
Stefano Chiodino <louderth@gmail.com>
2017-07-19 19:04:46 +0100

    makes client build its own password. Adjusts tests accordingly.

--- src/warheads/client.rs
+++ src/warheads/client.rs
@@ -1,30 +1,34 @@
 use warheads::api;
+use api_password_generator;
 
 pub struct Client {}
 
 impl Client {
-    pub fn launch<W: api::Api>(president_password: &str) -> Result<api::LaunchResponse, String> {
-        W::launch(president_password)
+    pub fn launch<W: api::Api>(user_password: &str) -> Result<api::LaunchResponse, String> {
+        let api_password = api_password_generator::build_password(&user_password);
+
+        W::launch(&api_password)
     }
 }
 
 #[cfg(test)]
 pub mod test {
-    use user_password_provider::UserPasswordProvider;
-    use user_password_provider::test::MockUserPasswordProvider;
-    use api_password_generator;
     use warheads::api;
     use warheads::api::Api;
     use warheads::client::Client;
+    use user_password_provider::UserPasswordProvider;
+    use user_password_provider::test::MockUserPasswordProvider;
+    use api_password_generator;
 
     #[derive(Debug)]
-    #[allow(dead_code)]
-    pub struct MockApi {}
+    pub struct ClientMockApi {}
 
-    impl Api for MockApi {
-        fn launch(password: &str) -> Result<api::LaunchResponse, String> {
-            let user_password = MockUserPasswordProvider::get_password();
-            let response = if password == api_password_generator::build_password(&user_password) {
+    impl Api for ClientMockApi {
+        fn launch(api_password: &str) -> Result<api::LaunchResponse, String> {
+            let mock_user_password = MockUserPasswordProvider::get_password();
+            let mock_api_password = api_password_generator::build_password(&mock_user_password);
+
+            let response = if api_password == mock_api_password {
                 api::LaunchResponse {
                     result: ::warheads::api::ResultState::Success,
                     message: "Success".to_string(),
@@ -42,7 +46,7 @@ pub mod test {
 
     #[test]
     fn test_launch_with_bad_password() {
-        let launch_response = Client::launch::<MockApi>("bad_password").unwrap();
+        let launch_response = Client::launch::<ClientMockApi>("bad_password").unwrap();
 
         assert_eq!(launch_response.result, api::ResultState::Failure);
         assert_eq!(launch_response.message, "Invalid Launch Code");
@@ -51,9 +55,7 @@ pub mod test {
     #[test]
     fn test_launch_with_good_password() {
         let user_password = MockUserPasswordProvider::get_password();
-        let launch_response =
-            Client::launch::<MockApi>(&api_password_generator::build_password(&user_password))
-                .unwrap();
+        let launch_response = Client::launch::<ClientMockApi>(&user_password).unwrap();
 
         assert_eq!(launch_response.result, api::ResultState::Success);
         assert_ne!(launch_response.message, "Invalid Launch Code");
